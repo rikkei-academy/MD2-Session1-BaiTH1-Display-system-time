91f5a5844baed78503c7e4e21eb8fae4d531bcd7
hunghx <120539776+hunghx@users.noreply.github.com>
2022-12-18 13:52:07 +0700

    Add files via upload

---
+++ BaiThucHanh1.1-Thoi-gian-he-thong/src/ra/academy/Test.java
@@ -0,0 +1,11 @@
+package ra.academy;
+
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+        //Write your code here
+        Date date = new Date(); // lấy ra thời gian hiện tại
+        System.out.println("Now is : "+date); // In thời gian ra màn hình
+    }
+}
